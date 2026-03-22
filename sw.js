8da42070191a5541acd0ca38add13e22ffab3d33
Callum Breyer <sonboiyt@gmail.com>
2026-03-22 19:49:52 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,17 @@
-const CACHE_NAME = 'macro-v1';
-const ASSETS = ['index.html', 'manifest.json'];
+const CACHE_NAME = 'ctrack-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/lucide@latest',
+  'https://unpkg.com/html5-qrcode'
+];
 
-self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
